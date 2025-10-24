3c0625b29987aa3f8765fae86d3399d95e6712ee
clayandrew824-spec <clayandrew824@gmail.com>
2025-10-24 14:35:06 +0100

    Flash and secret

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, session, redirect, f
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.secret_key = "******"
 CORS(app)
 
 USER = {
